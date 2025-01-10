6ca21cf8c335546ba94502735c36132098bed0cf
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-01-10 15:29:31 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../public/assets/css/app.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Ciao</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class MyNavbar extends Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default MyNavbar;
