c9db7199b66591f363f27e26a7448ed9635c49c6
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-01-10 15:56:46 +0100

    MyFooter converted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/assets/css/app.css';
 import MyNavbar from './components/MyNavbar';
+import MyFooter from './components/MyFooter';
 
 function App() {
   return (
     <>
       <div>
         <MyNavbar />
+        <MyFooter />
       </div>
     </>
   );
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,131 @@
+import { Component } from 'react';
+import { Facebook, Instagram, TwitterX, Youtube } from 'react-bootstrap-icons';
+
+class MyFooter extends Component {
+  render() {
+    return (
+      <footer>
+        <div className='row justify-content-center mt-5'>
+          <div className='col col-6'>
+            <div className='row'>
+              <div className='col mb-2'>
+                <Facebook className='footer-icon me-2'></Facebook>
+                <Instagram className='footer-icon me-2'></Instagram>
+                <TwitterX className='footer-icon me-2'></TwitterX>
+                <Youtube className='footer-icon'></Youtube>
+              </div>
+            </div>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg 4'>
+              <div className='col'>
+                <div className='row'>
+                  <div className='col footer-links'>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Audio and Subtitles
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Media Center
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Privacy
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Contact us
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='col'>
+                <div className='row'>
+                  <div className='col footer-links'>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Audio Description
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Investor Relations
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Legal Notices
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='col'>
+                <div className='row'>
+                  <div className='col footer-links'>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Help Center
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Jobs
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Cookie Preferences
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='col'>
+                <div className='row'>
+                  <div className='col footer-links'>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Gift Cards
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Terms of Use
+                      </a>
+                    </p>
+                    <p>
+                      <a href='#' alt='footer link'>
+                        Corporate Information
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col mb-2'>
+                <button
+                  type='button'
+                  className='btn btn-sm footer-button rounded-0 mt-3'
+                >
+                  Service Code
+                </button>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col mb-2 mt-2 copyright'>
+                © 1997-2023 Netflix, Inc.
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default MyFooter;
